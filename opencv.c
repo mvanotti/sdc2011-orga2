fc596ab4fdf4161b2d64ac81c02de468ea859d07
Marco Vanotti <mvanotti@dc.uba.ar>
2011-09-04 04:14:36 -0300

    Agregado el cambio de filtro y la pos de las ventanas

--- opencv.c
+++ opencv.c
@@ -6,10 +6,24 @@
 #include "filtros.h"
 
 
+const int max_filter = 4;
+
+/* filters es un arreglo de punteros a los filtros */
+const void (*filters[])(unsigned char *, unsigned char *, int, int, int) =
+	 {sobel_asm, prewitt_asm, roberts_asm, freichen_asm} ;
+
+const char* filter_names[] = {"sobel", "prewitt", "roberts", "freichen"};
+
+
 int main(void) {
+
+	int filter_index = 0;
+
+
 	CvCapture *capture = NULL;
 	IplImage *frame = NULL;	
 	IplImage *buffer = NULL;
+	IplImage *buffer2 = NULL;
 	int key;
 
 
@@ -30,13 +44,26 @@ int main(void) {
 		exit(1);
 	}
 
-	cvNamedWindow("webcam", CV_WINDOW_AUTOSIZE);
+	cvNamedWindow("filters", CV_WINDOW_AUTOSIZE);
+	cvNamedWindow("original", CV_WINDOW_AUTOSIZE);
+	frame = cvQueryFrame(capture);	
+	cvMoveWindow("original", frame->width + 10, 0);
+	cvMoveWindow("filters", 0, 0);
 	
+	key = -1;
 	while (key != 'q') {
 		frame = cvQueryFrame(capture);		
 
 		if (frame == NULL) {
 			break;
+		}
+
+
+		/* Si presionamos alguna tecla se cambia el filtro actual */
+		if (key != -1) {
+			filter_index += 1;
+			filter_index %= max_filter;
+			printf("Filtro actual:\t%s\n", filter_names[filter_index]);
 		}
 
 		/* Necesitamos pasar el frame capturado a blanco y negro
@@ -50,20 +77,21 @@ int main(void) {
 
 		cvCvtColor( frame, buffer, CV_RGB2GRAY);
 
-		frame = cvCreateImage( cvSize(frame->width, frame->height), 
+		buffer2 = cvCreateImage( cvSize(frame->width, frame->height), 
 				IPL_DEPTH_8U,
 				1);
 
-		sobel_asm (buffer->imageData, frame->imageData, buffer->height, 
-					buffer->width, buffer->widthStep);
+		filters[filter_index] (buffer->imageData, buffer2->imageData, 
+					buffer->height, buffer->width, buffer->widthStep);
 
 		/* Una vez aplicado el filtro, agregamos los fps a la imagen */
 		CvFont font;
     	cvInitFont(&font, CV_FONT_HERSHEY_SIMPLEX, 1.0, 1.0, 0, 1, CV_AA);
-    	cvPutText(frame, fpsstr , cvPoint(10, 30), 
+    	cvPutText(buffer2, fpsstr , cvPoint(10, 30), 
 			&font, cvScalar(255, 255, 255, 0));
 
-		cvShowImage("webcam", frame);
+		cvShowImage("original", frame);
+		cvShowImage("filters", buffer2);
 
 
 		/* Calculamos los fps */
@@ -71,21 +99,23 @@ int main(void) {
 		if (counter % 10 == 0) {
 			gettimeofday(&tv2, (void *) NULL);
 
-			fps       =  (10.0 * 1000000.0) / (difftime(tv2.tv_sec, tv.tv_sec) * 1000000 +  (tv2.tv_usec - tv.tv_usec));
+			fps = (10.0 * 1000000.0) / 
+					(difftime(tv2.tv_sec, tv.tv_sec) * 1000000 + 
+					 (tv2.tv_usec - tv.tv_usec));
+
 			gettimeofday(&tv, (void *) NULL);
-			printf("\b\b\b\b");
-			printf("fps: %.2f", fps);
 			sprintf(fpsstr, "fps: %.2f", fps);
 
 		}
 
 		key = cvWaitKey(1);
 		cvReleaseImage(&buffer);
-		cvReleaseImage(&frame);
+		cvReleaseImage(&buffer2);
 	}
 
 	cvReleaseCapture(&capture);
-	cvDestroyWindow("webcam");
+	cvDestroyWindow("original");
+	cvDestroyWindow("filters");
 	return 0; 	
 		
 }
