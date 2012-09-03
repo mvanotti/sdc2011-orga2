62f7fa803ed574acb4b1948542cbbd951c79a054
Matías Marquez <sir.incognita@gmail.com>
2012-09-03 01:54:07 -0300

    more filters declared

--- filtros/filtros.h
+++ filtros/filtros.h
@@ -4,4 +4,18 @@
 void rotar_c(unsigned char *src, unsigned char *dst, int h, int w, int src_row_size, int dst_row_size);
 void rotar_asm(unsigned char *src, unsigned char *dst, int h, int w, int src_row_size, int dst_row_size);
 
+void monocromatizar_c(unsigned char *src, unsigned char *dst, int h, int w, int src_row_size, int dst_row_size);
+void monocromatizar_asm(unsigned char *src, unsigned char *dst, int h, int w, int src_row_size, int dst_row_size);
+
+void scale2x_c(unsigned char *src, unsigned char *dst, int h, int w, int src_row_size, int dst_row_size); 
+void scale2x_asm(unsigned char *src, unsigned char *dst, int h, int w, int src_row_size, int dst_row_size);
+
+void sepia_c(unsigned char *src, unsigned char *dst, int h, int w, int src_row_size, int dst_row_size); 
+void sepia_asm(unsigned char *src, unsigned char *dst, int h, int w, int src_row_size, int dst_row_size);
+
+void smalltiles_c(unsigned char *src, unsigned char *dst, int h, int w, int src_row_size, int dst_row_size); 
+void smalltiles_asm(unsigned char *src, unsigned char *dst, int h, int w, int src_row_size, int dst_row_size);
+
+void blur_c(unsigned char *src, unsigned char *dst, int h, int w, int src_row_size, int dst_row_size); 
+void blur_asm(unsigned char *src, unsigned char *dst, int h, int w, int src_row_size, int dst_row_size);
 #endif /* !__FILTROS__H__ */
